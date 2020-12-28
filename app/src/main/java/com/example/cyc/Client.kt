59feb3e6dbfd27c38cf6656dc5e29123961c6441
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-28 06:32:48 +0530

    Initial Commit

--- app/src/main/java/com/example/cyc/Client.kt
+++ app/src/main/java/com/example/cyc/Client.kt
@@ -1,4 +1,28 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.cyc
 
-class Client {
+import android.os.AsyncTask
+import java.io.DataOutputStream
+import java.io.IOException
+import java.net.Socket
+
+class Client(var type:String="") : AsyncTask<Void, Void, Void>() {
+    private var IP_Address = "192.168.43.117"
+    override fun doInBackground(vararg p0: Void?): Void? {
+        if (type!=""){
+            createSocket(type)
+        }
+        return null
+    }
+    private fun  createSocket(str:String){
+        try {
+            val cli = Socket(IP_Address, 9999)
+            val toServer = cli.getOutputStream()
+            val dataOutputStream = DataOutputStream(toServer)
+            dataOutputStream.writeBytes(str)
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+    }
 }
--- app/src/main/java/com/example/cyc/MainActivity.kt
+++ app/src/main/java/com/example/cyc/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.cyc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private val shutdown: ImageButton = findViewById(R.id.shutdown)
-    private val play: ImageButton = findViewById(R.id.play)
+    private val music: ImageButton = findViewById(R.id.music)
     private val restart: ImageButton = findViewById(R.id.restart)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +16,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         shutdown.setOnClickListener {
-
+            val client = Client("shutdown")
+        }
+        shutdown.setOnLongClickListener {
+            createToast("shutdown")
+            return@setOnLongClickListener true
         }
 
-        play.setOnClickListener {
-
+        music.setOnClickListener {
+            val client = Client("music")
+        }
+        music.setOnLongClickListener {
+            createToast("play music")
+            return@setOnLongClickListener true
         }
 
         restart.setOnClickListener {
-
+            val client = Client("restart")
         }
+        restart.setOnLongClickListener {
+            createToast("restart")
+            return@setOnLongClickListener true
+        }
+    }
+    private fun  createToast(it:String){
+        Toast.makeText(this, it,Toast.LENGTH_LONG).show()
     }
 }
