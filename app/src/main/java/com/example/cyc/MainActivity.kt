9eeb28877a42046ac315c02e3209f9da987ffcde
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-28 06:11:21 +0530

    Initial Commit

--- app/src/main/java/com/example/cyc/MainActivity.kt
+++ app/src/main/java/com/example/cyc/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.cyc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private val shutdown: ImageButton = findViewById(R.id.shutdown)
+    private val play: ImageButton = findViewById(R.id.play)
+    private val restart: ImageButton = findViewById(R.id.restart)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        shutdown.setOnClickListener {
+
+        }
+
+        play.setOnClickListener {
+
+        }
+
+        restart.setOnClickListener {
+
+        }
     }
 }
